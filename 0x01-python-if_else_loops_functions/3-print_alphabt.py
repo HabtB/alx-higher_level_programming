f8aa18debcfc14e3e10f1e5d18d48e58ac4ad9f5
HabtB <email.Habtamu@gmail.com>
2022-11-30 17:22:59 +0000

    3-added

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for alpha in range(97, 123):
+for alpha in range(ord('a'), ord('z') + 1):
     if((chr(alpha) != 'e') and (chr(alpha) != 'q')):
-        print(chr(alpha), end="")
+        print('{:c}'.format(alpha), end="")
