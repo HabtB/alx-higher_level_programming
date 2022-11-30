c2a1e6f2952f33a3cad01d956ec1a59e2bda8ca8
HabtB <email.Habtamu@gmail.com>
2022-11-30 03:07:56 +0000

    1-last_digit added

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 
-if (number > 0):
+if(number >= 0):
     last_digit = number % 10
-elif (number < 0):
+elif(number < 0):
     last_digit = number % -10
 
 if(last_digit > 5):
