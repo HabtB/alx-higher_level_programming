a04714925d16699cd9fd6009e3326377cea61ba9
HabtB <email.Habtamu@gmail.com>
2022-12-08 02:56:14 +0000

    10 updated

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     best = 0
     key = None
-    if a dictionary is None:
+    if a_dictionary is None:
         return key
     for i in a_dictionary:
-        if a_dictionary[i] > best
-        best = a_dictionary[i]
-        key = i
+        if a_dictionary[i] > best:
+            best = a_dictionary[i]
+            key = i
     return key
