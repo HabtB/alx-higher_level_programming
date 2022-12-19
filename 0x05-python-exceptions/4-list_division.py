0d8be1269cc4341bf9cb340d481dc9ab21ea3bb6
HabtB <email.Habtamu@gmail.com>
2022-12-19 16:19:33 +0000

    4 modified

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,6 @@
 def list_division(my_list_1, my_list_2, list_length):
     nlist = []
     for count in range(0, list_length):
-        result = 0
         try:
             result = (my_list_1[count] / my_list_2[count])
         except ZeroDivisionError:
