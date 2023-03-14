b635e2a31531be4d5a9cd8b4dfc6b6f65b071296
HabtB <email.Habtamu@gmail.com>
2023-03-14 01:26:11 +0000

    1-multi_language updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 /* This code prints JavaScript is amazing on the screen*/
-let myVar = 'C is fun'
-console.log(myVar)
-myVar = 'Python is cool'
-console.log(myVar)
-myVar = 'JavaScript is amazing'
+let myVar = 'C is fun \n Python is cool \n JavaScript is amazing'
 console.log(myVar)
