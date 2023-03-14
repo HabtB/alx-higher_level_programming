e14ea4a7307f7976c4b0536c5c170f089df0d960
HabtB <email.Habtamu@gmail.com>
2023-03-14 01:11:46 +0000

    0 added to fit in the check

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-/* This code prints JavaScript is amazing on the screen*/
-let myVar = "JavaScript is amazing"
-console.log(myVar)
+
+let myVar = "JavaScript is amazing";
+console.log(myVar);
