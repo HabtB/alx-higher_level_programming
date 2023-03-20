4d4d9b4038569a3c6aa187d06fe7028672f428d9
HabtB <email.Habtamu@gmail.com>
2023-03-20 02:24:40 +0000

    9 updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 This script lists all State objects
 that contain the letter `a`
 from the database `hbtn_0e_6_usa`.
@@ -15,13 +15,14 @@ if __name__ == "__main__":
     from the database.
     """
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    query = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
-#    query = session.query(State).filter(State.name.contains('a'))
+    query = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id)
     for state in query.all():
         print("{}: {}".format(state.id, state.name))
