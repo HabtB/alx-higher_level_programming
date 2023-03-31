79df8e7ac6d7a8b9cf02334659edba0855a78dda
HabtB <email.Habtamu@gmail.com>
2023-03-31 02:35:35 +0000

    5-post params updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # #Takes in a URL, sends a POST request to the passed URL, and displays
-curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLS"
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
