fd5891a73758d66ae80ed400981583f8f22db42f
HabtB <email.Habtamu@gmail.com>
2023-03-31 02:12:59 +0000

    4-header updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-# Takes in a URL as an argument, sends a GET request with 
-# A header variable X-School-User-Id must be sent with the value 98
-# to the URL, and displays the body of the response
+#!/bin/bashi
+# Takes URL as an arg, sends a GET request with a header variable X-School-User-Id
+# must be sent with the value 98 to the URL, and displays response
 curl -sH "X-School-User-Id: 98" "$1"
 
