ac8d5d6ef966ca19eec0b49053cf24a86f08fec5
HabtB <email.Habtamu@gmail.com>
2023-03-31 23:29:37 +0000

    header added

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = requests.get(url)
-    x_request_id = req.headers.get('X-Request-Id')
-    print(x_request_id)
+    req = requests.get(sys.argv[1])
+    print(req.headers.get('X-Request-Id'))
