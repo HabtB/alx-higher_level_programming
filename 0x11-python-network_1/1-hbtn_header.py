b723fba3ebab6213a2e48db4cf2b139edc42ed49
HabtB <email.Habtamu@gmail.com>
2023-03-31 17:15:45 +0000

    1-hbtn header updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@
 from urllib import request
 import sys
 
-
-with request.urlopen(sys.argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == '__main__':
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
