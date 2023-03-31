424dd6ed85ce3474ba70fc32172e7d37503f5ddb
HabtB <email.Habtamu@gmail.com>
2023-03-31 21:25:55 +0000

    2 post_email added

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,14 +4,13 @@ Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 import urllib.request
-import urllib.parse 
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = urllib.parse.urlencode({'email':sys.argv[2]}).encode('ascii')
-    
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
-    
