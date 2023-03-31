d3c0448b4095435342692037a9218d650aa4a291
HabtB <email.Habtamu@gmail.com>
2023-03-31 21:17:38 +0000

    2 post_email added

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script takes in URL, sends post request with email parameter, displays body"""
+import urllib.request
+import urllib.parse 
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = urllib.parse.urlencode({'email':sys.argv[2]}).encode('ascii')
+    
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
