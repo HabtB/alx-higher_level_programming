56d43a058bc3053f5bad61a35db34e811c423895
HabtB <email.Habtamu@gmail.com>
2023-04-01 02:10:34 +0000

    10-my_github updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     uname = sys.argv[1]
     psswd = sys.argv[2]
 
-    body = requests.get("https://api.github.com/users/{username}", auth=(uname, psswd))
+    url = f'https://api.github.com/users/{uname}'
+    body = requests.get(url, auth=(uname, psswd))
     if body.status_code == 200:
         print(body.json().get('id'))
