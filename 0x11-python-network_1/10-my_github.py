9ac1bfd39a709b35179ad784ef953277dd9b6bcb
HabtB <email.Habtamu@gmail.com>
2023-04-01 02:44:33 +0000

    10-my_github updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" 
-A script that takes github credentials and uses the GitHubAPI
-to display id
-"""
-import sys
+'''
+Takes your Github credentials (username and password) and uses
+the Github API to display your id
+'''
+
 import requests
+import sys
 
 if __name__ == "__main__":
-    uname = sys.argv[1]
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
     token = sys.argv[2]
     headers = {'Authorization': 'token {}'.format(token)}
-    url = 'https://api.github.com/user'
-    body = requests.get(url, headers=headers)
-    if body.status_code == 200:
-        print((body.json()).get('id'))
+    req = requests.get(url, headers=headers)
+    print(req.json().get('id'))
