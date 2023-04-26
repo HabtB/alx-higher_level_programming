5f965dbbdfcc60aa77a92b6c95e5dfce33d94f79
HabtB <email.Habtamu@gmail.com>
2023-04-26 16:38:15 +0000

    101 updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,18 +4,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
-    return;
   }
-  JSON.parse(body).characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      console.log(JSON.parse(body).name);
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, function (err, response, body) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
     });
-  });
+  }
 });
